ab81d2f93dec3f56c59d76c6559d5297ee7c59db
Meron <merronmuche@gmail.com>
2024-03-06 11:08:53 +0300

    convert the sql data to dataframe

--- process_data.py
+++ process_data.py
@@ -5,9 +5,14 @@ from postgres_connection import connect_to_postgres
 
 
 def get_df():
-    # engine = connect_to_postgres()
+    engine = connect_to_postgres()
 
-    # sql_query = 'SELECT * FROM xdr_data'
-    df = pd.read_csv('data.csv')
+    sql_query = 'SELECT * FROM xdr_data'
+    df = pd.read_sql(sql_query, con=engine)
 
     return df
+
+if __name__ == "__main__":
+
+    df = get_df()
+    
