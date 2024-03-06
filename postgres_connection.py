33d11ff3dc4ef02cc4de3b2f3ee4c84eede4de6f
Meron <merronmuche@gmail.com>
2024-03-06 11:11:11 +0300

    change port to connect to postgress

--- postgres_connection.py
+++ postgres_connection.py
@@ -2,15 +2,13 @@
 from sqlalchemy import create_engine
 
 
-print(__name__)
-
 def connect_to_postgres():
 
     database_name = 'telecom'
     table_name= 'xdr_data'
 
     connection_params = { "host": "localhost", "user": "postgres", "password": "postgres",
-                        "port": "5432", "database": database_name}
+                        "port": "5434", "database": database_name}
 
     engine = create_engine(f"postgresql+psycopg2://{connection_params['user']}:{connection_params['password']}@{connection_params['host']}:{connection_params['port']}/{connection_params['database']}")
 
